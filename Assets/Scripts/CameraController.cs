e24911c0494259668a8a3d869fdf1161ba1f2413
Chris <chris.hoffmann84@gmail.com>
2017-12-17 19:04:32 +0100

    tutorial version implemented

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject player;
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		if (!player)
+			return;
+		
+		offset = transform.position - player.transform.position;
+		Debug.Log (offset);
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () { //runs every frame, but only after everything has been updated in Update()
+		if (!player)
+			return;
+
+		transform.position = player.transform.position + offset;
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+	public float speed = 3;
+	public Text pickupCountText;
+	public Text winText;
+
+	private Rigidbody rb;
+	private int pickupCount = 0;
+
+	private void UpdatepickupCountText() 
+	{
+		pickupCountText.text = "Score: " + pickupCount.ToString();
+
+		if (pickupCount >= 12) 
+		{
+			winText.text = "YOU WIN!";
+		}
+	}
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody> ();
+		UpdatepickupCountText ();
+		winText.text = "";
+	}
+
+	void Update ()
+	{ //will be called when a new frame is rendered
+		
+	}
+
+	void FixedUpdate ()
+	{ //will be called on physics update (=> physics code goes here
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		rb.AddForce (speed * new Vector3 (moveHorizontal, 0f, moveVertical));
+	}
+
+	void OnTriggerEnter (Collider other)
+	{
+		if (other.gameObject.CompareTag ("pickup")) 
+		{
+			other.gameObject.SetActive (false);
+			pickupCount++;
+			UpdatepickupCountText ();
+		}
+	}
+}

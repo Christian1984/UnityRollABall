cbcbb08d4ea04e60123e4e4abf34e38032c9a75b
DESKTOP-1MHKANA\Christian <chris.hoffmann84@gmail.com>
2017-12-19 21:27:58 +0100

    - added a very simple VR shooter scene to familiarize myself with how that stuff works

---
+++ Assets/Scripts/VRController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRController : MonoBehaviour {
+
+    public AudioClip audioFx;
+    private AudioSource audioSource;
+
+    public Transform gunBarrelTransform;
+
+	// Use this for initialization
+	void Start () {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.clip = audioFx;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (OVRInput.Get(OVRInput.Button.SecondaryIndexTrigger))
+        {
+            Debug.Log("trigger");
+            audioSource.Play();
+            RaycastGun();
+        }		
+	}
+
+    private void RaycastGun()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(gunBarrelTransform.position, - gunBarrelTransform.right, out hit))
+        {
+            if (hit.collider.gameObject.CompareTag("target"))
+            {
+                Destroy(hit.collider.gameObject);
+            }
+        }
+    }
+}
